442584abfbf4bcbd4a12a962d1393092468cb0c3
woodecode <wangzhiyu.work@outlook.com>
2022-10-22 11:00:51 +0800

    clean cache files

--- 0.no_lvgl/project/MDK-ARM/project/multi_button.d
+++
@@ -1,4 +0,0 @@
-project/multi_button.o: ..\UserApp\libs\support\button\multi_button.c \
-  ..\UserApp\libs\support\button\multi_button.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h
